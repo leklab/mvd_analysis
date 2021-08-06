20419cfcb08a153f297b7ee722920672cc52d294
monkollek <monkol.lek@yale.edu>
2021-08-06 09:29:36 -0400

    fixed paths in launch_cromwell.sh

--- launch_cromwell.sh
+++ launch_cromwell.sh
@@ -6,6 +6,6 @@
 
 java -Dconfig.file=wdl/slurm.conf -jar \
 /gpfs/ycga/project/lek/shared/tools/jars/cromwell-56.jar run \
-wdl/gatk4_multisample/Multisample_Fastq_to_Gvcf_GATK4.wdl \
--i wdl/gatk4_multisample/Multisample_Fastq_to_Gvcf_GATK4_inputs_hg38.json \
+wdl/Multisample_Fastq_to_Gvcf_GATK4.wdl \
+-i wdl/Multisample_Fastq_to_Gvcf_GATK4_inputs_hg38.json \
 -o wdl/cromwell.options
