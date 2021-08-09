be6b91cb098161abafcb93f6fe29def8f0006c73
monkollek <monkol.lek@yale.edu>
2021-08-09 15:07:08 -0400

    copied over joint calling wdl from cromwell_wdl repo

---
+++ launch_cromwell_jointgt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -c 4
+#SBATCH -J launch_cromwell
+#SBATCH --mem=16000
+
+java -Dconfig.file=wdl/slurm.conf -jar \
+/gpfs/ycga/project/lek/shared/tools/jars/cromwell-56.jar run \
+scripts/Multisample_jointgt_GATK4.wdl \
+-i scripts/Multisample_jointgt_GATK4_inputs_hg38.json \
+-o wdl/cromwell.options
